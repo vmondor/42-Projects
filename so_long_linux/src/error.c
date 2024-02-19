e5f6c1c7f811dd5190d4c78c3967edf76cd3010d
Valentin Mondor <vmondor@bess-f3r3s4.clusters.42paris.fr>
2024-02-19 17:54:04 +0100

    add so_long_linux corriger

--- so_long_linux/src/error.c
+++ so_long_linux/src/error.c
@@ -6,7 +6,7 @@
 /*   By: vmondor <vmondor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 15:02:16 by vmondor           #+#    #+#             */
-/*   Updated: 2024/02/19 12:35:53 by vmondor          ###   ########.fr       */
+/*   Updated: 2024/02/19 17:52:59 by vmondor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	error(char *str, char **tab)
 {
 	if (!str)
 		str = " ";
-	ft_printf("Error\n%s\n", str);
+	ft_printf("Error\n");
+	write(2, str, ft_strlen(str));
 	if (tab)
 		free_tab(tab);
 	exit (1);
